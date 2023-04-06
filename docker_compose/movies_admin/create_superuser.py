3d03875312b00c7e781b2686e0f6ab431ba99fd0
rsof14 <sofia.rytik@gmail.com>
2023-04-06 15:01:20 +0300

    Добавлена реализация API

--- docker_compose/movies_admin/create_superuser.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-
-adm = User.objects.filter(username='admin')
-adm.delete()
-usr = User.objects.create_superuser('admin' 'sofya140202@mail.ru' 'password')
-usr.save()
--- docker_compose/movies_admin/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python manage.py makemigrations
-python manage.py migrate
-python create_superuser.py
